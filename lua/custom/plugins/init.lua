40925c91e018b35cac5479890a8cb92b52f6bdbc
Michal Mieczkowski <mieczkowski@fastmail.com>
2024-11-03 11:22:22 +0100

    neotree setup and shortcuts

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,7 +38,37 @@ return {
       vim.o.background = 'dark' -- or 'light' for light version
       vim.cmd 'colorscheme gruvbox'
 
-      vim.keymap.set('n', '<leader>tb', ':set background=<C-r>=&background == "dark" ? "light" : "dark"<CR><CR>', { desc = 'Toggle background dark/light' })
+      vim.keymap.set(
+        'n',
+        '<leader>tb',
+        ':set background=<C-r>=&background == "dark" ? "light" : "dark"<CR><CR>',
+        { noremap = true, silent = true, desc = '[T]oggle [B]ackground dark/light' }
+      )
+    end,
+  },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require('neo-tree').setup {
+        close_if_last_window = true,
+        popup_border_style = 'rounded',
+        enable_git_status = true,
+        enable_diagnostics = true,
+        window = {
+          width = 30,
+          mappings = {
+            ['<space>'] = 'none', -- disable space mapping
+          },
+        },
+      }
+      vim.keymap.set('n', '<space>tt', ':Neotree toggle<CR>', { noremap = true, silent = true, desc = '[T]oggle Neo-[T]ree' })
     end,
   },
 }
